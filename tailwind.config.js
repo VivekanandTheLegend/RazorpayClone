f5c3a54f03d2edb67147b4f9b47002e75c04de0c
vivekanand-123 <tiwarivicky9212@gmail.com>
2023-08-24 19:35:14 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily:{
+        mullish:["Mulish","sans-serif"],
+      },
+      colors:{
+        deepBlue:"#02042a",
+        lightBlue:"#2b84ea",
+        lightBlue300:"#4b94ed",
+        lightBlue500: "#8b72e7",
+        greenLight : "#61ceab",
+        grayText: "#818597",
+        lightGray: "#e2e2e2",
+        grayBlue: "#344a6c", 
+      },
+
+    },
+  },
+  plugins: [],
+};
